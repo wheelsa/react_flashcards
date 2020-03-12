0a6c00d123eff3de3c2e1dc38324a10362162296
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-12 09:26:21 -0600

    started setting up app, established functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{ 
+  state = {
+    flashcards: [
+      { id: 1, question: "What is your favorite color?", Answer: "Green, sometimes blue" },
+      { id: 2, question: "What is the air speed velocity of a swallow?", answer: "African or European?" }
+    ],
+    showForm: false
+  };
+
+  addCard = card => {
+    let newCard = {...card, id: Math.floor(Math.random()*100000)};
+
+    this.setState({newCard, ...this.state.flashcards})
+  }
+
+  deleteCard = id => {
+    const { flashcards } = this.state;
+
+    const newCards = flashcards.filter(card => card.id !== id);
+
+    this.setState({
+      flashcards: newCards
+    });
+  }
+
+  toggleForm =() => {
+    this.setState({
+      showForm: !this.state.showForm
+    })
+  }
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     FlashCards
     </div>
   );
+}
+
 }
 
 export default App;
